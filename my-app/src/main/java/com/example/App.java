bc3af7c9726db559b9a664b76d94c62a90422593
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-23 10:15:30 +0200

    Finish SpringCore-Ex1

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Environment: " + System.getProperty("env"));    
+    public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+            HelloService helloService = context.getBean("helloService", HelloService.class);
+
+            System.out.println(helloService.getMessage());
+
+            if (context instanceof ClassPathXmlApplicationContext) {
+                ((ClassPathXmlApplicationContext) context).close();
+            }
+        } catch (Exception e) {
+            System.err.println("Error loading Spring context: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
---
+++ my-app/src/main/java/com/example/HelloService.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HelloService {
+    
+    public String getMessage() {
+        return "Hello, Spring!";
+    }
+}
