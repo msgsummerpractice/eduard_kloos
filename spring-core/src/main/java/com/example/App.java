6e0e311be464b3548cd515582f9708d14a015cec
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-24 08:35:08 +0200

    Clean up repository and ignore generated files

---
+++ spring-core/src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan(basePackages = "com.example")
+public class App 
+{
+    public static void main(String[] args) {
+       AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(App.class);
+
+        AnimalService service = context.getBean(AnimalService.class);
+        service.makeAnimalSpeak();
+
+        context.close();
+    }
+}
