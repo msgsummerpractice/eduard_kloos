4d0154a34e4154736e61db17f234cdb23acfb1a9
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-24 15:34:54 +0200

    Use Lombok annotations for User model

--- spring-boot/src/main/java/com/example/demo/model/User.java
+++ spring-boot/src/main/java/com/example/demo/model/User.java
@@ -1,45 +1,23 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class User {
 
     private Long id;
     private String name;
     private String email;
     private String password;
-
-    
-    public User(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long setId(Long id) {
-        return this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
     
 }
