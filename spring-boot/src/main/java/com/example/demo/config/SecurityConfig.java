6b8e6ae5090a3e4596caf6d45536d92b87821afc
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-28 12:31:45 +0200

    Configure custom login page with Spring Security

--- spring-boot/src/main/java/com/example/demo/config/SecurityConfig.java
+++ spring-boot/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,19 +24,29 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
+
             .authorizeHttpRequests(auth -> auth
 
-                    .requestMatchers("/api/users/**")
-                    .authenticated()
+                    .requestMatchers(
+                            "/login",
+                            "/css/**",
+                            "/js/**"
+                    )
+                    .permitAll()
 
                     .anyRequest()
                     .authenticated()
             )
-            .httpBasic(Customizer.withDefaults());
+
+            .formLogin(
+                    form -> form
+                            .loginPage("/login")
+                            .permitAll()
+            );
 
 
         return http.build();
