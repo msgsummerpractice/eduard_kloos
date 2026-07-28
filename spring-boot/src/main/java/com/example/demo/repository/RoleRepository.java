5f5041b6d11bb0a4992e091afdd51ace2ab7ee14
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-28 13:05:15 +0200

    Initialize users and roles for database authentication

--- spring-boot/src/main/java/com/example/demo/repository/RoleRepository.java
+++ spring-boot/src/main/java/com/example/demo/repository/RoleRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Role;
 
-
-
 public interface RoleRepository extends JpaRepository<Role, Long>{
 
     Optional<Role> findByName(String name);
