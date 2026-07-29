71b7d4d611c09edb6565be7483b62a02a44845fe
Eduard-Andreas Kloos <soluicius@yahoo.com>
2026-07-29 11:23:52 +0200

    feat: add Angular Material responsive navigation bar

--- frontend/training-app/src/app/app.ts
+++ frontend/training-app/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('training-app');
